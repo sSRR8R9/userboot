4dd15e05a3b29365b88a2ef3af0cbb6f1020ea1c
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-10-25 17:22:05 +0300

    Create خط غامق.py

---
+++ sbb_b/plugins/خط غامق.py
@@ -0,0 +1,45 @@
+# DaR - Karar
+# © matrix Team 2023
+# ها شعدك داخل ع الملف تريد تخمط ؟ ابو زربة لهل درجة فاشل  
+from telethon import events
+from jepthon import jepiq
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..core.managers import edit_delete
+from telethon import functions
+
+@sbb_b.ar(admin_cmd(pattern="خط غامق"))
+async def btext(event):
+    isbold = gvarstatus("bold")
+    if not isbold:
+        addgvar ("bold", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل الخط الغامق بنجاح ✓**")
+        return
+
+    if isbold:
+        delgvar("bold")
+        await edit_delete(event, "**᯽︙ تم اطفاء الخط الغامق بنجاح ✓ **")
+        return
+
+@sbb_b.ar(admin_cmd(pattern="خط رمز"))
+async def btext(event):
+    isramz = gvarstatus("ramz")
+    if not isramz:
+        addgvar ("ramz", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل الخط الرمز بنجاح ✓**")
+        return
+
+    if isramz:
+        delgvar("ramz")
+        await edit_delete(event, "**᯽︙ تم اطفاء الخط الرمز بنجاح ✓ **")
+        return
+
+@sbb_b.ar(events.NewMessage(outgoing=True))
+async def reda(event):
+    isbold = gvarstatus("bold")
+    if isbold:
+        await event.edit(f"**{event.message.message}**")
+@sbb_b.ar(events.NewMessage(outgoing=True))
+async def lMl10l(event):
+    isramz = gvarstatus("ramz")
+    if isramz:
+        await event.edit(f"`{event.message.message}`")
