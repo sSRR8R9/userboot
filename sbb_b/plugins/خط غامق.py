885c37c12760ff484865d4b9c64b274214470c10
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-10-25 17:48:24 +0300

    Update خط غامق.py

--- sbb_b/plugins/خط غامق.py
+++ sbb_b/plugins/خط غامق.py
@@ -2,7 +2,7 @@
 # © matrix Team 2023
 # ها شعدك داخل ع الملف تريد تخمط ؟ ابو زربة لهل درجة فاشل  
 from telethon import events
-from sbb_b import razan
+from jepthon import jepiq
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from ..core.managers import edit_delete
 from telethon import functions
