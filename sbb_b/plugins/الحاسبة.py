70433893cda9f42b6baa49c2fdb67ae2e91a372d
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-10-25 12:26:56 +0300

    Update الحاسبة.py

--- sbb_b/plugins/الحاسبة.py
+++ sbb_b/plugins/الحاسبة.py
@@ -5,7 +5,7 @@ from telethon.events import CallbackQuery, InlineQuery
 
 from sbb_b import sbb_b
 
-# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @sbb_b  ~ @RR7PP
+# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @MQQDD  ~ @ZBBBBB
 from ..core.decorators import check_owner
 
 CALC = {}
@@ -41,7 +41,7 @@ lst.append([Button.inline("=", data="calc=")])
 @sbb_b.ar_cmd(pattern="الحاسبة(?:\s|$)([\s\S]*)")
 async def icalc(e):
     if e.client._bot:
-        return await e.reply("**الحاسبة العلمية لسورس جمثون\n @sbb_b**", buttons=lst)
+        return await e.reply("**الحاسبة العلمية لسورس مآتركس\n @ZBBBBB**", buttons=lst)
     results = await e.client.inline_query(Config.TG_BOT_USERNAME, "calc")
     await results[0].click(e.chat_id, silent=True, hide_via=True)
     await e.delete()
@@ -57,12 +57,12 @@ async def inlinecalc(event):
     ) and string == "calc":
         event.builder
         calc = event.builder.article(
-            "Calc", text="**الحاسبة العلمية لسورس جمثون\n @sbb_b**", buttons=lst
+            "Calc", text="**الحاسبة العلمية لسورس مآتركس\n @ZBBBBB**", buttons=lst
         )
         await event.answer([calc])
 
 
-# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @sbb_b  ~ @RR7PP
+# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @MQQDD  ~ @ZBBBBB
 @sbb_b.tgbot.on(CallbackQuery(data=re.compile(b"calc(.*)")))
 @check_owner
 async def _(e):  # sourcery no-metrics
@@ -73,7 +73,7 @@ async def _(e):  # sourcery no-metrics
         if CALC.get(user):
             CALC.pop(user)
         await e.edit(
-            "**الحاسبة العلمية لسورس جمثون\n @sbb_b**",
+            "**الحاسبة العلمية لسورس مآتركس\n @ZBBBBB**",
             buttons=[Button.inline("افتح مره اخرى", data="recalc")],
         )
     elif x == "C":
@@ -128,7 +128,7 @@ async def _(e):  # sourcery no-metrics
         await e.answer(str(x))
 
 
-# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @sbb_b  ~ @RR7PP
+# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @MQQDD  ~ @ZBBBBB
 @sbb_b.tgbot.on(CallbackQuery(data=re.compile(b"recalc")))
 @check_owner
 async def _(e):
@@ -157,4 +157,4 @@ async def _(e):
     tultd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
     lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
     lst.append([Button.inline("=", data="calc=")])
-    await e.edit("**الحاسبة العلمية لسورس مآتركس\n @sbb_b**", buttons=lst)
+    await e.edit("**الحاسبة العلمية لسورس مآتركس\n @ZBBBBB**", buttons=lst)
