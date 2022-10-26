123c2eab3c61ec305f87a6c645ec3afe40802746
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-10-26 19:39:45 +0300

    Update __init__.py

--- sbb_b/__init__.py
+++ sbb_b/__init__.py
@@ -14,12 +14,12 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.2.0"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "سورس جمثون <https://github.com/jmthonen/sbb_b0>"
-__copyright__ = f" حقوق سورس جمثون (C) 2020 - 2022  {__author__}"
+__author__ = "سورس ماتركس <https://github.com/bbhh1/userboot>"
+__copyright__ = f" حقوق سورس ماتركس (C) 2022 - 2023  {__author__}"
 
 sbb_b.version = __version__
 sbb_b.tgbot.version = __version__
-LOGS = logging.getLogger("سورس جمثون")
+LOGS = logging.getLogger("سورس ماتركس")
 bot = sbb_b
 
 StartTime = time.time()
